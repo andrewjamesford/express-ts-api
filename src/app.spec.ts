d6898216091c2674c21151b9b6f5fbd51cd56d49
Andrew Ford <andrew@andrewford.co.nz>
2023-11-19 23:52:15 +0000

    Updated devcontainer Ran prettier formatting Added formatting settings Moved results to constant

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -3,8 +3,8 @@ import request from "supertest";
 import app from "./app";
 
 describe("Tasks Router", () => {
-
   it("should get hello world", (done) => {
+    // Expected value to test
     const expected = { message: "Hello World" };
     request(app)
       .get("/")
@@ -12,5 +12,4 @@ describe("Tasks Router", () => {
       .expect("Content-Type", /json/)
       .expect(200, expected, done);
   });
-
 });
